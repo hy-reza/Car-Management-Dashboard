9d84aeb8d0df1ff66fb750574dda245d28ece527
hy-reza <hy.rezaalfanda@gmail.com>
2022-10-05 14:13:37 +0700

    create table

---
+++ migrations/20221005065830-create-cars-table.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("cars", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      car_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      car_size: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      car_photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      car_price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
